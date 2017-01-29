a69a3f8ec542fde9cab996b2b6838290853bfad3
skasti <stig-rune.skansgard@bekk.no>
2017-01-29 12:36:43 +0100

    Use NAV-PVT instead of NAV-VELNED, as this gives time and position as well.

--- datalogger/datalogger.ino
+++ datalogger/datalogger.ino
@@ -15,38 +15,81 @@ const uint16_t VALUE_COUNT = 8;
 
 struct logLine
 {
-    uint32_t micros;
-    uint16_t values[VALUE_COUNT];
+    uint32_t  micros;
+    uint16_t  speed;
+    uint16_t  sAcc; //Speed accuracy extimate
+    long      lon;
+    long      lat;
+    long      alt;
+    unsigned long   hAcc; //Horizontal accuracy estimate
+    unsigned long   vAcc; //Vertical accuracy estimate
+    uint16_t  values[VALUE_COUNT];
 };
 
 
 const unsigned char UBX_HEADER[] = { 0xB5, 0x62 };
 
-struct NAV_VELNED {
-  unsigned char cls;
-  unsigned char id;
-  unsigned short len;
-  unsigned long iTOW;
-  long velN;
-  long velE;
-  long velD;
-  unsigned long spd;
-  unsigned long gSpd;
-  long hdg;
-  unsigned long sAcc;
-  unsigned long hAcc;
+struct NAV_PVT {
+  unsigned char   cls;
+  unsigned char   id;
+  unsigned short  len;
+  unsigned long   iTOW; //GPS Time of week
+  uint16_t        year;
+  uint8_t         month;
+  uint8_t         day;
+  uint8_t         hour;
+  uint8_t         min;
+  uint8_t         sec;
+  
+  unsigned char   valid; 
+  //valid Bitflags: 
+  //4 = fullyResolved, 
+  //2 = validDate, 
+  //1 = validTime
+
+  unsigned long   tAcc; //Time accuracy extimate (UTC)
+  long            nano; //fraction of second range.
+  uint8_t         fixType;
+  //fixType values:
+  //0: no fix
+  //1: dead reckoning only
+  //2: 2D-fix
+  //3: 3D-fix
+  //4: GNSS + dead reckoning
+  //5: Time only
+
+  unsigned char   flags; //fix status flags
+  unsigned char   flags2; //additional flags
+  uint8_t         numSV; //Number of satellites
+  long            lon; //Longitude. Scaling: 1e-7
+  long            lat; //Latitude. Scaling: 1e-7
+  long            height; //Height above ellipsoid
+  long            alt; //Height above mean sea level
+  unsigned long   hAcc; //Horizontal accuracy estimate
+  unsigned long   vAcc; //Vertical accuracy estimate
+  long            velN; //NED North Velocity
+  long            velE; //NED East Velocity
+  long            velD; //NED Down Velocity
+  long            gSpeed; //Ground speed (2-D)
+  long            headMot; //Heading of motion
+  unsigned long   sAcc; //Speed accuracy estimate
+  unsigned long   headAcc; //Heading accuracy estimate
+  uint16_t        pDOP; //Position DOP
+  uint8_t         reserved1[6]; //Reserved
+  long            headVeh; //heading of vehicle (2-D)
+  uint8_t         reserved2[4]; //Reserved
 };
 
-NAV_VELNED velned;
-unsigned long groundSpeed = 0;
+NAV_PVT     pvtBuffer;
+NAV_PVT     pvt;
 
 int flushInterval = 5000;
 int flushCounter = 0;
 
 void calcChecksum(unsigned char* CK) {
   memset(CK, 0, 2);
-  for (int i = 0; i < (int)sizeof(NAV_VELNED); i++) {
-    CK[0] += ((unsigned char*)(&velned))[i];
+  for (int i = 0; i < (int)sizeof(NAV_PVT); i++) {
+    CK[0] += ((unsigned char*)(&pvtBuffer))[i];
     CK[1] += CK[0];
   }
 }
@@ -54,7 +97,7 @@ void calcChecksum(unsigned char* CK) {
 bool processGPS() {
   static int fpos = 0;
   static unsigned char checksum[2];
-  const int payloadSize = sizeof(NAV_VELNED);
+  const int payloadSize = sizeof(NAV_PVT);
 
   while ( Serial.available() ) {
     byte c = Serial.read();
@@ -66,7 +109,7 @@ bool processGPS() {
     }
     else {
       if ( (fpos-2) < payloadSize )
-        ((unsigned char*)(&velned))[fpos-2] = c;
+        ((unsigned char*)(&pvtBuffer))[fpos-2] = c;
 
       fpos++;
 
@@ -128,9 +171,6 @@ void setup() {
     filename = date + "-" + counter;
   }
 
-  //filename = "data.log";
-
-
   if (sdCardInitialized) {
     logFile = SD.open(filename + extension,FILE_WRITE);
 
@@ -149,7 +189,7 @@ void setup() {
 
 void loop() {  
   if ( processGPS() ) {
-    groundSpeed = velned.gSpd;
+    pvt = pvtBuffer;
   }
 
   unsigned long ms = micros();
@@ -157,12 +197,17 @@ void loop() {
   struct logLine line;
   line.micros = ms;
 
-  for (int i = 0; i < 6; i++) {
+  line.speed = pvt.gSpeed;
+  line.sAcc = pvt.sAcc;
+  line.lon = pvt.lon;
+  line.lat = pvt.lat;
+  line.alt = pvt.alt;
+  line.hAcc = pvt.hAcc;
+  line.vAcc = pvt.vAcc;
+
+  for (int i = 0; i < VALUE_COUNT; i++) {
     line.values[i] = analogRead(inputs[i]);
   }
-
-  line.values[6] = (uint16_t)(velned.sAcc);
-  line.values[7] = (uint16_t)(groundSpeed);
 
   logFile.write((const uint8_t *)&line, sizeof(line));
 
