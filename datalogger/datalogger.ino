e7167ff368b95248f47be03e89be336d2adcdfeb
Stig-Rune Skansgård <stig-rune.skansgard@bekk.no>
2019-02-10 16:43:14 +0100

    Removed nano-gpu-stuff

--- datalogger/datalogger.ino
+++ datalogger/datalogger.ino
@@ -4,9 +4,7 @@
 #include "UBX.h"
 #include "states.h"
 #include "gps.h"
-#include "nanogpu-client.h"
 #include "SparkFunMLX90614.h"
-#include "menu.h"
 #include <EEPROM.h>
 #include <WString.h>
 #include "nextionDisplay.h"
@@ -52,21 +50,17 @@ int inputs[] = { A0,A1,A2,A3,A4,A5,A6,A7 };
 int sdCardPin = 53;
 bool sdCardInitialized = false;
 
-int upPin = 30;
-int downPin = 32;
-int enterPin = 34;
-int toggleUIPin = 36;
 int toggleLoggingPin = 40;
+int enterPin = 40;
 int redLedPin = 45;
 int greenLedPin = 42;
 int blueLedPin = 44;
 
-int toggleUIState, prevToggleUIState, toggleLoggingState, prevToggleLoggingState;
+int toggleLoggingState, prevToggleLoggingState;
 unsigned long toggleLoggingStart = 0;
 bool loggingToggled = false;
 
 File logFile;
-uint8_t signalStrength = 0x00;
 const uint16_t VALUE_COUNT = 14;
 
 struct LogLine
@@ -88,7 +82,6 @@ LogLine line;
 NAV_PVT     pvt;
 
 Gps gps;
-NanoGpuClient gpuClient;
 NextionDisplay display;
 
 String pollValuesCmd = String("pollValues");
@@ -122,42 +115,9 @@ char* channelComponentNames[] = {
   "a4", "a5", "a6", "a7", //Second set of analogs
 };
 
-MenuItem menuItems[] = {
-  MenuItem(0x1000, 0x0000, 0x1000, "Start Log"),
-    MenuItem(0x1100, 0x1000, 0x0000, "OK"),
-    MenuItem(0x1200, 0x1000, 0x0000, "Abort"),
-  MenuItem(0x2000, 0x0000, 0x2000, "Calibrate"),
-    MenuItem(0x2100, 0x2000, 0x2100, "Channel"),
-      MenuItem(0x2110, 0x2100, 0x2000, "Ch 1"),
-      MenuItem(0x2120, 0x2100, 0x2000, "Ch 2"),
-      MenuItem(0x2130, 0x2100, 0x2000, "Ch 3"),
-      MenuItem(0x2140, 0x2100, 0x2000, "Ch 4"),
-      MenuItem(0x2150, 0x2100, 0x2000, "Ch 5"),
-      MenuItem(0x2160, 0x2100, 0x2000, "Ch 6"),
-      MenuItem(0x2170, 0x2100, 0x2000, "Ch 7"),
-      MenuItem(0x2180, 0x2100, 0x2000, "Ch 8"),
-    MenuItem(0x2200, 0x2000, 0x2200, "Start"),
-      MenuItem(0x2210, 0x2200, 0x2000, "OK"),
-      MenuItem(0x2220, 0x2200, 0x2000, "Abort"),
-    MenuItem(0x2300, 0x2000, 0x2300, "Stop"),
-      MenuItem(0x2310, 0x2300, 0x2000, "OK"),
-      MenuItem(0x2320, 0x2300, 0x2000, "Abort"),
-    MenuItem(0x2400, 0x2000, 0x2400, "Save"),
-      MenuItem(0x2410, 0x2400, 0x2000, "OK"),
-      MenuItem(0x2420, 0x2400, 0x2000, "Abort"),
-    MenuItem(0x2500, 0x2000, 0x2500, "Reset"),
-      MenuItem(0x2510, 0x2500, 0x2000, "OK"),
-      MenuItem(0x2520, 0x2500, 0x2000, "Abort"),
-    MenuItem(0x2600, 0x2000, 0x0000, "BACK"),
-  MenuItem(0x3000, 0x0000, 0x0000, "Toggle UI"),
-  MenuItem(0x4000, 0x0000, 0x0000, "Auto ON")
-};
-
-Menu menu(upPin,downPin,enterPin, menuItems, 28);
 
 void sendDebug(char text[])
 {
-  gpuClient.sendStatus(text);
   Serial.println(text);
   display.debug(text);
 }
@@ -172,20 +132,15 @@ void setup() {
 
   gps.setup();
 
-  gpuClient.setup();
   display.setup();
 
   Serial.begin(9600);
 
   EEPROM.write(AUTOSTART_EEPROM, 0);  
   autoStart = (EEPROM.read(AUTOSTART_EEPROM) == 0);
-  updateAutoStartMenuItem(); 
 
   digitalWrite(13,LOW);
 
-  gpuClient.sendSignal(signalStrength);
-  
-  gpuClient.sendMode(STATUSTEXT);
   delay(10);  
   sendDebug("IR INIT");
 
@@ -217,9 +172,6 @@ void setup() {
     pinMode(inputs[i], INPUT);  
   }
 
-  pinMode(toggleUIPin, INPUT_PULLUP);
-  pinMode(toggleLoggingPin, INPUT_PULLUP);
-  toggleUIState = digitalRead(toggleUIPin);
   updateToggleLoggingButton();
 
   digitalWrite(13,HIGH);
@@ -229,14 +181,6 @@ void setup() {
   initSD();
   analogWrite(redLedPin,LOW);
   analogWrite(greenLedPin, 40);
-}
-
-void updateAutoStartMenuItem()
-{
-  if (autoStart)
-    menu.setText(0x4000, "Auto ON");
-  else
-    menu.setText(0x4000, "Auto OFF");
 }
 
 void getGPSFix()
@@ -310,22 +254,6 @@ void updateIRTemps()
     currentIR = 0;
 }
 
-bool draw()
-{
-  if (ms > nextDrawTime)
-  {
-    gpuClient.sendValues(line.values);
-    nextDrawTime = ms + drawInterval;
-
-    if (isLogging)
-      nextDrawTime = ms + loggingDrawInterval;
-
-    return true;
-  }
-
-  return false;
-}
-
 bool initLogFile()
 {
   if (!sdCardInitialized)
@@ -385,9 +313,6 @@ void toggleLogging()
     logFile.flush();
     logFile.close();
     isLogging = false;
-    isMenu = true;
-    gpuClient.sendMode(STATUSTEXT);
-    menu.setText(0x1000, "Start Log");
     digitalWrite(13, LOW);
   }
   else
@@ -395,17 +320,9 @@ void toggleLogging()
     if (!initLogFile())
       return;
 
-    gpuClient.sendMode(VALUES);
-    isMenu = false;
     isLogging = true;
-    menu.setText(0x1000, "Stop Log");
     digitalWrite(13, HIGH);
   }
-}
-
-void toggleUI()
-{
-  isMenu = !isMenu;
 }
 
 void channelSelect(int channel)
@@ -418,45 +335,6 @@ void channelSelect(int channel)
   }
 
   calibrateIndex = channel;
-}
-
-void startCalibration()
-{
-  if (isCalibrating)
-    return;
-    
-  isCalibrating = true;
-  gpuClient.sendCalibrate(calibrateIndex);
-}
-
-void stopCalibration()
-{
-  if (!isCalibrating)
-    return;
-    
-  isCalibrating = false;
-  gpuClient.sendCalibrate(0xFF);
-}
-
-void storeCalibration()
-{    
-  gpuClient.sendStoreCalibration(calibrateIndex);
-}
-
-void resetCalibration()
-{    
-  gpuClient.sendResetCalibration(calibrateIndex);
-}
-
-void updateToggleUIButton()
-{
-  prevToggleUIState = toggleUIState;
-  toggleUIState = digitalRead(toggleUIPin);
-
-  if (toggleUIState == LOW && prevToggleUIState == HIGH)
-  {
-    toggleUI();
-  }
 }
 
 void updateToggleLoggingButton()
@@ -537,7 +415,6 @@ void toggleAutoStart()
   else
     EEPROM.write(AUTOSTART_EEPROM, 0xFF);
 
-  updateAutoStartMenuItem();
   sendAutoStart();
 }
 
@@ -592,7 +469,7 @@ void loop()
     nextLogTime = ms + logInterval;
   }
 
-  if (!draw() || !isLogging) {
+  if (!isLogging) {
     updateIRTemps();
   }
 
@@ -619,90 +496,9 @@ void loop()
   {
     nextInputUpdate = ms + inputUpdateInterval;
 
-    updateToggleUIButton();
     updateToggleLoggingButton();  
     updateAutoStart();
-
-    if (isMenu)
-    {
-      uint16_t menuAction = menu.update();
-
-      switch(menuAction)
-      {
-        case 0x1100: //Toggle logging
-          toggleLogging();
-          break;
-        case 0x3000: //Toggle UI
-          toggleUI();
-          break;
-        case 0x4000: //Toggle Autostart
-          toggleAutoStart();
-          break;
-
-        case 0x2110: //Select Ch 1
-          channelSelect(6);
-          break;
-        case 0x2120: //Select Ch 2
-          channelSelect(7);
-          break;
-        case 0x2130: //Select Ch 3
-          channelSelect(8);
-          break;
-        case 0x2140: //Select Ch 4
-          channelSelect(9);
-          break;
-        case 0x2150: //Select Ch 5
-          channelSelect(10);
-          break;
-        case 0x2160: //Select Ch 6
-          channelSelect(11);
-          break;
-        case 0x2170: //Select Ch 7
-          channelSelect(12);
-          break;
-        case 0x2180: //Select Ch 8
-          channelSelect(13);
-          break;
-
-        case 0x2210: //Start Calibration
-          startCalibration();
-          break;
-
-        case 0x2310: //Stop Calibration
-          stopCalibration();
-          break;
-
-        case 0x2410: //Stop Calibration
-          storeCalibration();
-          break;
-
-        case 0x2510: //Stop Calibration
-          resetCalibration();
-          break;
-        
-        default:
-          break;
-      }
-      
-      if (gpuClient.getMode() != STATUSTEXT)
-      {
-        gpuClient.sendMode(STATUSTEXT);
-      }
-    } 
-    else
-    {
-      if (gpuClient.getMode() != VALUES)
-        gpuClient.sendMode(VALUES);
-    }
   }
-
-  if (isMenu)
-    menu.render(gpuClient);
-
-  if (isMenu)
-    digitalWrite(13, HIGH);
-  else
-    digitalWrite(13, LOW);
 
   if (ms > nextBlink)
   {
@@ -731,28 +527,5 @@ void loop()
        digitalWrite(ledPin, LOW);
 
     nextBlink = ms + blinkInterval;
-  }
-
-  if (ms > nextSignal)
-  {
-    nextSignal = ms + signalInterval;
-
-    uint32_t hAcc = pvt.hAcc / 1000;
-
-    if (hAcc <= 1)
-      signalStrength = 0xFF;
-    else if (hAcc <= 2)
-      signalStrength = 0xE0;
-    else if (hAcc <= 4)
-      signalStrength = 0xC0;
-    else if (hAcc <= 8)
-      signalStrength = 0xA0;
-    else if (hAcc <= 16)
-      signalStrength = 0x80;
-    else if (pvt.fixType >= 2)
-      signalStrength = 0x10;
-
-    gpuClient.sendSignal(signalStrength);
-  }
-  
+  }  
 }
